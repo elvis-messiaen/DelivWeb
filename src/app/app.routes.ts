be3bd44dc1a0af332864e1946947d67a10a0e5db
elvis messiaen <elvism72@gmail.com>
2024-10-28 17:48:42 +0100

    Mise en place du header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './layout/notfound/notfound.component';
+import { DetailsComponent } from './layout/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: '**', component: NotfoundComponent },
+];
