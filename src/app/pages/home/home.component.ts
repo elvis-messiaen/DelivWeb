eb1bf011071eba02809c0c9bf58b1c7a96305213
elvis messiaen <elvism72@gmail.com>
2024-11-15 10:45:24 +0100

    correctif

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,68 +21,6 @@ export class HomeComponent implements OnInit {
   private lineLength: number = 50;
   public numberofEntry: number = 0;
 
-  public pieChartData: ChartData<'pie'> = {
-    labels: [],
-    datasets: [
-      {
-        label: 'Médailles remportées',
-        data: [],
-        backgroundColor: [
-          'rgba(137, 73, 81, 0.6)',
-          'rgba(106, 40, 65, 0.6)',
-          'rgba(166, 193, 227, 0.6)',
-          'rgba(112, 145, 214, 0.6)',
-          'rgba(172, 219, 238, 0.6)',
-          'rgba(134, 107, 145, 0.6)',
-        ],
-      },
-    ],
-  };
-
-  public pieChartOptions: ChartConfiguration<'pie'>['options'] = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-        position: 'top' as const,
-        labels: {
-          padding: 20,
-        },
-      },
-      tooltip: {
-        callbacks: {
-          label: (tooltipItem) => {
-            const index = tooltipItem.dataIndex;
-            const label = tooltipItem.label;
-            const data = this.pieChartData.datasets[0].data[index];
-
-            if (this.olympics && index >= 0 && index < this.olympics.length) {
-              this.hoveredCountryId = this.olympics[index].id;
-              this.numberofEntry = this.olympics[index].participations.length;
-            } else {
-              this.hoveredCountryId = undefined;
-              this.numberofEntry = 0;
-            }
-
-            this.numberofJOs = data;
-            this.cdr.markForCheck();
-            return `${label}: ${data} médailles`;
-          },
-        },
-      },
-    },
-    layout: {
-      padding: {
-        top: 20,
-        left: 20,
-        right: 20,
-        bottom: 20,
-      },
-    },
-    aspectRatio: 0.8,
-  };
-
   constructor(
     private olympicService: OlympicService,
     private cdr: ChangeDetectorRef,
@@ -125,6 +63,83 @@ export class HomeComponent implements OnInit {
     );
   }
 
+  public pieChartData: ChartData<'pie'> = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Médailles remportées',
+        data: [],
+        backgroundColor: [
+          'rgba(137, 73, 81, 0.6)',
+          'rgba(106, 40, 65, 0.6)',
+          'rgba(166, 193, 227, 0.6)',
+          'rgba(112, 145, 214, 0.6)',
+          'rgba(172, 219, 238, 0.6)',
+          'rgba(134, 107, 145, 0.6)',
+        ],
+      },
+    ],
+  };
+
+  onChartHover(event: { event: MouseEvent; active: {}[] }): void {
+    console.log('Chart Hover Event:', event);
+  }
+
+  public pieChartOptions: ChartConfiguration<'pie'>['options'] = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: true,
+        position: 'nearest',
+        bodyFont: {
+          size: 14,
+        },
+        displayColors: false,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        callbacks: {
+          label: (tooltipItem) => {
+            const index = tooltipItem.dataIndex;
+            const label = tooltipItem.label;
+            const data = this.pieChartData.datasets[0].data[index];
+
+            if (
+              this.olympics &&
+              index >= 0 &&
+              this.pieChartData.labels &&
+              index < this.pieChartData.labels.length
+            ) {
+              this.hoveredCountryId = this.olympics[index].id;
+              this.numberofEntry = this.olympics[index].participations.length;
+            } else {
+              this.hoveredCountryId = undefined;
+              this.numberofEntry = 0;
+            }
+
+            this.numberofJOs = data;
+            this.cdr.markForCheck();
+            return `${label}: ${data} médailles`;
+          },
+          title: (tooltipItems) => {
+            return 'Détails des Médailles';
+          },
+        },
+      },
+    },
+    layout: {
+      padding: {
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20,
+      },
+    },
+    aspectRatio: 1, // Ajuster le ratio pour qu'il soit plus carré
+  };
+
   onChartClick(): void {
     if (this.hoveredCountryId) {
       this.router.navigate(['/details', this.hoveredCountryId]);
@@ -144,6 +159,9 @@ export class HomeComponent implements OnInit {
       }) => {
         const ctx = chart.ctx;
         const datasets = chart.data.datasets;
+
+        // Vérifie si nous sommes en mode mobile
+        const isMobile = window.innerWidth <= 430;
 
         datasets.forEach(
           (dataset: { data: number[] }, datasetIndex: number) => {
@@ -173,39 +191,54 @@ export class HomeComponent implements OnInit {
                 const labelX = model.x;
                 const labelY = model.y;
 
-                const colors = [
-                  'rgba(137, 73, 81, 1)',
-                  'rgba(106, 40, 65, 1)',
-                  'rgba(166, 193, 227, 1)',
-                  'rgba(112, 145, 214, 1)',
-                  'rgba(172, 219, 238, 1)',
-                  'rgba(134, 107, 145, 1)',
-                ];
+                if (!isMobile) {
+                  const colors = [
+                    'rgba(137, 73, 81, 1)',
+                    'rgba(106, 40, 65, 1)',
+                    'rgba(166, 193, 227, 1)',
+                    'rgba(112, 145, 214, 1)',
+                    'rgba(172, 219, 238, 1)',
+                    'rgba(134, 107, 145, 1)',
+                  ];
 
-                ctx.save();
-                ctx.strokeStyle = colors[index];
-                ctx.lineWidth = 2;
-                ctx.font = '16px Arial';
+                  ctx.save();
+                  ctx.strokeStyle = colors[index];
+                  ctx.lineWidth = 2;
+                  ctx.font = '16px Arial';
 
-                const angle = (model.startAngle + model.endAngle) / 2;
-                const xOuter = model.x + Math.cos(angle) * model.outerRadius;
-                const yOuter = model.y + Math.sin(angle) * model.outerRadius;
-                const xLineEnd =
-                  model.x + Math.cos(angle) * (model.outerRadius + 150);
-                const yLineEnd = yOuter;
+                  const angle = (model.startAngle + model.endAngle) / 2;
+                  const xOuter = model.x + Math.cos(angle) * model.outerRadius;
+                  const yOuter = model.y + Math.sin(angle) * model.outerRadius;
+                  const xLineEnd =
+                    model.x + Math.cos(angle) * (model.outerRadius + 150);
+                  const yLineEnd = yOuter;
 
-                ctx.beginPath();
-                ctx.moveTo(xOuter, yOuter);
-                ctx.lineTo(xLineEnd, yLineEnd);
-                ctx.stroke();
+                  ctx.beginPath();
+                  ctx.moveTo(xOuter, yOuter);
+                  ctx.lineTo(xLineEnd, yLineEnd);
+                  ctx.stroke();
 
-                const textX = xLineEnd + (angle > Math.PI ? -10 : 10);
-                const textY = yLineEnd;
+                  const textX = xLineEnd + (angle > Math.PI ? -10 : 10);
+                  const textY = yLineEnd;
 
-                ctx.textAlign = angle > Math.PI ? 'right' : 'left';
+                  ctx.textAlign = angle > Math.PI ? 'right' : 'left';
 
-                ctx.fillText(`${label}`, textX, textY);
-                ctx.restore();
+                  ctx.fillText(`${label}`, textX, textY);
+                  ctx.restore();
+                } else {
+                  // Affiche les noms des pays directement à côté des sections en mode mobile
+                  const angle = (model.startAngle + model.endAngle) / 2;
+                  const xLabel =
+                    model.x + Math.cos(angle) * (model.outerRadius * 0.9); // Réduire de 10%
+                  const yLabel =
+                    model.y + Math.sin(angle) * (model.outerRadius * 0.9); // Réduire de 10%
+
+                  ctx.save();
+                  ctx.font = '14px Arial'; // Augmenter la taille de la police
+                  ctx.textAlign = 'center';
+                  ctx.fillText(`${label}`, xLabel, yLabel);
+                  ctx.restore();
+                }
               }
             );
           }
